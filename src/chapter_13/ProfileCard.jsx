29031de2aadd08fc2acd4e3d58e3124e4502c965
jomo85 <jomo85@vntgcorp.com>
2022-08-23 13:40:57 +0900

    chapter13

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+  return (
+    <Card title="jomo85" backgroundColor="#4ea04e">
+      <p>안녕하세요. 이종호입니다.</p>
+      <p>리액트 공부!!</p>
+    </Card>
+  );
+}
+
+export default ProfileCard;
